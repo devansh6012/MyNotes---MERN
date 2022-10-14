d5136289bee107f06b7a5113fde7e0ece86d9a9d
Devansh Gupta <devansh.g6012@gmail.com>
2022-10-14 15:51:18 +0530

    updated the mongoURI

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const mongoURI ="mongodb://localhost:27017/inotes"
-
+// const mongoURI ="mongodb://localhost:27017/inotes"
+const mongoURI ="mongodb+srv://devansh:devansh@cluster0.lbowzpv.mongodb.net/?retryWrites=true&w=majority"
 const connectToMongo = () =>{
     mongoose.connect(mongoURI, () => {
         console.log("Connected to Mongo");
